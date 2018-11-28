03bbc08e5ec4aac2a8f338c6c66af279b4de1c31
Pontus Erlingsson <erpo16@nti.umea>
2018-11-28 13:15:27 +0100

    typ klar

--- bil-lista.py
+++ bil-lista.py
@@ -13,3 +13,19 @@ while meny !=4:
             print(line)
         print("")
         f.close()
+    elif meny == 2:
+        message = input("skriv in ett bilmärke")
+        f = open ('lista.txt', 'a+')
+        f.write("\n" + message)
+        f.close()
+    elif meny == 3:
+        try:
+            delete = int(input("är du säker tryck 1 för att forsätta. tryck 0 för att avbryta "))
+        except:
+            print("använd en siffra")
+        if delete == 1:
+            f = open('lista.txt', 'w')
+            f.write("")
+            f.close()
+        if delete == 0:
+            print("avbrutet")
